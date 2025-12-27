5ffdfa2a11c3ec7944a6d2800426823e885b9f01
José <biel16211@gmail.com>
2025-12-27 17:46:23 -0400

    build: Add src/game.c for compilation test

---
+++ src/game.c
@@ -0,0 +1,12 @@
+#include "game.h"
+#include <ncurses.h>
+
+void init_game(GameState *game) {}
+
+void process_input(GameState *game) {}
+
+void update_state(GameState *game) {}
+
+void render_game(GameState *game) {}
+
+void cleanup_game(GameState *game) {}
