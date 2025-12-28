924616c5e02aa55ca64062a5e5a1252315c1ed70
José <biel16211@gmail.com>
2025-12-28 00:28:18 -0400

    feat: Implement game state initialization and ncurses setup

--- src/game.c
+++ src/game.c
@@ -1,8 +1,23 @@
 #include "game.h"
 #include <ncurses.h>
 
-void init_game(GameState *game) {}
+void init_game(GameState *game) {
 
+  initscr();
+  nodelay(stdscr, TRUE);
+  curs_set(0);
+
+  // Init the main grid whit 0
+  for (int i = 0; i < MAX_HEIGHT; i++) {
+    for (int k = 0; k < MAX_WIDTH; k++) {
+      game->grid[i][k] = 0;
+    }
+  }
+
+  game->grid[1][1] = 1;
+}
+
+// No input for now
 void process_input(GameState *game) {}
 
 void update_state(GameState *game) {}
